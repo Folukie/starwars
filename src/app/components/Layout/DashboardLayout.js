d2078c42dd8bd65fe0bf99791546a0e3c0d44c51
Foluke Odusanya <odusanya.folukemi@gmail.com>
2024-01-23 20:56:32 +0100

    add state management

--- src/app/components/Layout/DashboardLayout.js
+++ src/app/components/Layout/DashboardLayout.js
@@ -8,7 +8,7 @@ const DashboardLayout = ({ children, goBackHref }) => {
       <Header goBackHref={goBackHref} />
       <div className="md:flex">
         <SideNav />
-        <div className=" h-full py-8 text-primary-dark flex flex-col flex-1 overflow-y-auto ml-0 md:ml-16 w-11/12">
+        <div className=" h-full py-8 text-primary-dark flex flex-col flex-1 overflow-y-auto  md:ml-16 w-11/12 mx-auto ">
           {children}
         </div>
       </div>
